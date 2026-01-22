e0e0a0fc9dd9dbfbf7654d542e011ef4fcdddddb
Darius15222 <dariusonofrei.2014@gmail.com>
2026-01-22 19:08:51 +0200

    ssh and https available to only admin_cidr ip variable

--- security.tf
+++ security.tf
@@ -1,32 +1,39 @@
 resource "aws_security_group" "lab_sg" {
   name        = "lab-sg"
-  description = "Allow all internal traffic"
+  description = "Allow all internal traffic and restricted external access"
   vpc_id      = aws_vpc.main.id
 
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [var.admin_cidr] 
   }
+
   ingress {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [var.admin_cidr]
   }
+
   ingress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["10.0.0.0/16"]
   }
+
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  
+  tags = merge(local.common_tags, {
+    Name = "lab-security-group"
+  })
 }
 
 resource "aws_default_security_group" "default" {
